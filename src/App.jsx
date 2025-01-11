88eff2b002358578dcca99a67d386c952eb13853
Justin Cordova <justinavodroc@gmail.com>
2025-01-11 16:19:21 -0500

    Fixed submit button css Added space for working logo on top left

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import { useState } from "react";
-import ExpenseForm from './components/ExpenseForm';
-import ExpenseList from './components/ExpenseList';
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
 import "./App.css";
 
 function App() {
   const [expenses, setExpenses] = useState([]);
 
   const addExpenseFunc = (expense) => {
-    setExpenses([...expenses, expense])
+    setExpenses([...expenses, expense]);
   };
-  
+
   return (
     <>
       <div className="App">
-        <h1>Expense Tracker</h1>
+        <header className="header">
+          {/* <img src="/logo.png" alt="Logo" className="logo" /> */}
+          <h1>Expense Tracker</h1>
+        </header>
         <ExpenseForm addExpense={addExpenseFunc} />
         <ExpenseList expenses={expenses} />
       </div>
-
-      
     </>
   );
 }
